66a13f31658d591cfeb080a712f56e2922e21750
AntonKomarov <anton.komarov116@gmail.com>
2020-04-16 13:23:25 +0300

    practice

--- src/by/it/komarov/jd01_12/TaskA1.java
+++ src/by/it/komarov/jd01_12/TaskA1.java
@@ -5,20 +5,47 @@ import java.util.Iterator;
 import java.util.List;
 
 public class TaskA1 {
-    private List<Integer> grades =new ArrayList<>();
+    @Override
+    public String toString() {
+        return "TaskA1{" +
+                "grades=" + grades +
+                '}';
+    }
 
-    private void clearBad(List<Integer> grades){
-        grades.removeIf(element -> element < 4);
+    private List<Integer> grades = new ArrayList<>();
+
+
+    public List<Integer> rand(List<Integer> array, int size){
+        for (int i = 0; i < size; i++) {
+            array.add((int) (1 + Math.random() * 5));
+        }
+        return array;
+    }
+
+    public void clearBad(List<Integer> grades){
+        Iterator<Integer> iterator = grades.iterator();
+        while(iterator.hasNext()) {
+            if (iterator.next() <= 3) {
+                iterator.remove();
+            }
+        }
     }
 
     public static void main(String[] args) {
-        TaskA1 instance = new TaskA1();
-        for (int i = 0; i < 20; i++) {
-            int grade = 1 + (int) (Math.random() * 10);
-            instance.grades.add(grade);
-        }
-        System.out.println(instance.grades);
-        instance.clearBad(instance.grades);
-        System.out.println(instance.grades);
+        TaskA1 a1 = new TaskA1();
+        a1.rand(a1.getGrades(), 20);
+        System.out.println(a1.getGrades());
+
+        a1.clearBad(a1.getGrades());
+        System.out.println(a1.getGrades());
+
+    }
+
+    public List<Integer> getGrades() {
+        return grades;
+    }
+
+    public void setGrades(List<Integer> grades) {
+        this.grades = grades;
     }
 }
